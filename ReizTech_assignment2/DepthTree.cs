b62a7b4a151c3653c381e4775bc05183a9d4245c
Ina Bardhollari <inabardhollari8@hotmail.com>
2022-11-16 18:46:15 +0100

    Add project files.

---
+++ ReizTech_assignment2/DepthTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ReizTech_assignment2
+{
+    class DepthTree
+    {
+        
+        static void Main(string[] args)
+        {
+            ///<summary>Tree Structure</summary>
+            TreeNode<int> root = new TreeNode<int>(1);
+            TreeNode<int> c1 = new TreeNode<int>(2);
+            TreeNode<int> c2 = new TreeNode<int>(2);
+            TreeNode<int> c3 = new TreeNode<int>(3);
+            TreeNode<int> c4 = new TreeNode<int>(3);
+            TreeNode<int> c5 = new TreeNode<int>(3);
+            TreeNode<int> c6 = new TreeNode<int>(3);
+            TreeNode<int> c7 = new TreeNode<int>(4);
+            TreeNode<int> c8 = new TreeNode<int>(4);
+            TreeNode<int> c9 = new TreeNode<int>(4);
+            TreeNode<int> c10 = new TreeNode<int>(5);
+
+            root.setChild(c1); //setting the children of the root which is 1
+            root.setChild(c2);
+
+            //setting the other children to other nodes
+            c1.setChild(c3);
+
+            c2.setChild(c4);
+            c2.setChild(c5);
+            c2.setChild(c6);
+
+            c4.setChild(c7);
+
+            c5.setChild(c8);
+            c5.setChild(c9);
+
+            c8.setChild(c10);
+
+            // Calculating depth of the tree by calling the method and printing the answer
+            Console.WriteLine("MAX DEPTH OF THE TREE IS : " +root.maxDepth(root));
+        }
+
+    }
+}
---
+++ ReizTech_assignment2/TreeNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReizTech_assignment2
+{
+    public class TreeNode<T>
+    {
+        //declaring a list because we have a non-binary tree and one of the parent have more than 2 child
+        private List<TreeNode<T>> children = new List<TreeNode<T>>();  
+        private T data = default(T);
+        public TreeNode(T data)
+        {
+            this.data = data;
+        }
+        public List<TreeNode<T>> getChildren()
+        {
+            return children;
+        }
+
+        public void setChild(TreeNode<T> children)
+        {
+            this.children.Add(children);
+        }
+
+        public int maxDepth(TreeNode<T> root) 
+        {
+            if (root == null) return 0;
+            int h = 0;
+
+            foreach (TreeNode<T> n in root.getChildren())
+            {
+                h = Math.Max(h, maxDepth(n)); 
+            }
+            return h + 1;  //widht=height+1
+        }
+     
+    }
+}
